d2777b8dcbbf95f0f75690f964fbe923d583d7ca
AlyszaZealand <als012@edu.zealand.dk>
2026-02-23 13:01:01 +0100

    Din mor

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
 
-        for (int i = 0; i>5;i++){
+        for (int i = 0; i<8;i++){
             System.out.println("kage");
         }
 
